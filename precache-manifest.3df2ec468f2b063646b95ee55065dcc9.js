061b87fc61a042bc509e221a1c53b20e440d9e2c
shivamthakur0010 <demonking0010@gmail.com>
2023-04-26 21:33:00 +0530

    Updates

---
+++ precache-manifest.3df2ec468f2b063646b95ee55065dcc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c52f987810be65ae5e2c28e232dfc73",
+    "url": "/image-compressor/index.html"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/image-compressor/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/image-compressor/static/js/2.69fef3bc.chunk.js"
+  },
+  {
+    "revision": "a0ca72aba3af08952d5e",
+    "url": "/image-compressor/static/js/main.5ed05a1a.chunk.js"
+  },
+  {
+    "revision": "a2f10b2e2d0f4c040449",
+    "url": "/image-compressor/static/js/runtime-main.9014142f.js"
+  }
+]);
